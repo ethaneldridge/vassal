0cb9aad7d47726332206cd00bfd0fa1aeea88b49
rodneykinney <rodneykinney@67b53d14-2c14-4ace-a08f-0dab2b34000c>
2005-01-31 07:39:30 +0000

    Zone Editor fixes
    git-svn-id: https://svn.code.sf.net/p/vassalengine/svn/trunk@469 67b53d14-2c14-4ace-a08f-0dab2b34000c

--- src/VASSAL/build/module/map/boardPicker/board/mapgrid/Zone.java
+++ src/VASSAL/build/module/map/boardPicker/board/mapgrid/Zone.java
@@ -55,410 +55,6 @@ public class Zone extends AbstractConfigurable implements GridContainer {
     return name;
   }
 
-/*
-  public static class PathConfig implements ConfigurerFactory {
-    public Configurer getConfigurer(
-      AutoConfigurable c,
-      String key,
-      String name) {
-      return new PathConfigurer(key, name);
-    }
-
-    public class PathConfigurer
-      extends StringConfigurer
-      implements ActionListener {
-
-      JButton modButton;
-
-      public PathConfigurer(String key, String name) {
-        super(key, name);
-      }
-
-      public java.awt.Component getControls() {
-        if (p == null) {
-          super.getControls();
-          nameField.setEnabled(false);
-          modButton = new JButton("Modify Shape");
-          modButton.addActionListener(this);
-          p.add(modButton);
-        }
-
-        return p;
-      }
-
-       //Modify button has been clicked, pop up the path editor
-      public void actionPerformed(ActionEvent arg0) {
-        PolygonConfigurer config = new PolygonConfigurer(editZone, this);
-        config.setVisible(true);
-
-        //
-        if (!config.isCancelled()) {
-          editZone.setPath(config.getPoints());
-          nameField.setText(calcPath(editZone.getPolygon()));
-        }
-      }
-    }
-
-    public class PolygonConfigurer extends JDialog implements ActionListener {
-
-      ArrayList points = new ArrayList();
-      private JPanel topPanel;
-      private JPanel headerPanel;
-      private JPanel bodyPanel;
-      private JPanel optionPanel;
-      private JPanel scrollPanel;
-      private JPanel visPanel;
-      private PolygonVisualizer visualizer;
-      private JPanel buttonPanel;
-      private JScrollPane scroll;
-      private ArrayList rowPanels;
-      private int selectedRow = -1;
-      private boolean cancelled = false;
-      private Zone myZone;
-      private PathConfigurer myPathConfigurer;
-
-      private JButton okButton, canButton;
-
-      protected PolygonConfigurer(Zone zone, PathConfigurer pathConfig) {
-
-        myZone = zone;
-        myPathConfigurer = pathConfig;
-
-        Polygon poly = zone.getPolygon();
-        setModal(true);
-
-        setTitle(zone.getName() + " - Edit Shape");
-        setBackground(Color.gray);
-
-        if (zone.pathPoints == null) {
-          points = new ArrayList();
-        }
-        else {
-          points = new ArrayList(zone.pathPoints);
-        }
-
-        headerPanel = new JPanel();
-
-        scrollPanel = new JPanel();
-        scrollPanel.setLayout(new BoxLayout(scrollPanel, BoxLayout.Y_AXIS));
-        scrollPanel.setBorder(BorderFactory.createLineBorder(Color.black));
-        rowPanels = new ArrayList();
-
-        for (int i = 0; i < points.size(); i++) {
-          Row line = new Row(i, points, this);
-          scrollPanel.add(line);
-          rowPanels.add(i, line);
-        }
-
-        scroll =
-          new JScrollPane(
-            scrollPanel,
-            JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
-            JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-
-        buttonPanel = new JPanel();
-        okButton = new JButton(OK_ACTION);
-        okButton.addActionListener(this);
-        canButton = new JButton(CANCEL_ACTION);
-        canButton.addActionListener(this);
-        buttonPanel.add(okButton);
-        buttonPanel.add(canButton);
-
-        optionPanel = new JPanel();
-        optionPanel.setLayout(new BoxLayout(optionPanel, BoxLayout.Y_AXIS));
-        addAButton(UP_ACTION, optionPanel);
-        addAButton(DOWN_ACTION, optionPanel);
-        addAButton(ADD_ACTION, optionPanel);
-        addAButton(REMOVE_ACTION, optionPanel);
-
-        visPanel = new JPanel();
-        visualizer = new PolygonVisualizer(points);
-        visPanel.add(visualizer);
-
-        bodyPanel = new JPanel();
-        bodyPanel.setLayout(new BoxLayout(bodyPanel, BoxLayout.X_AXIS));
-        bodyPanel.add(Box.createRigidArea(new Dimension(5, 0)));
-        bodyPanel.add(optionPanel);
-        bodyPanel.add(Box.createRigidArea(new Dimension(5, 0)));
-        bodyPanel.add(scrollPanel);
-        bodyPanel.add(Box.createRigidArea(new Dimension(5, 0)));
-        bodyPanel.add(visPanel);
-        bodyPanel.add(Box.createRigidArea(new Dimension(5, 0)));
-
-        getContentPane().add(headerPanel, BorderLayout.PAGE_START);
-        getContentPane().add(bodyPanel, BorderLayout.CENTER);
-        getContentPane().add(buttonPanel, BorderLayout.PAGE_END);
-
-        pack();
-      }
-
-      private void setZone(Zone z) {
-        myZone = z;
-      }
-
-      private Zone getZone() {
-        return myZone;
-      }
-
-      private void addAButton(String text, Container container) {
-        JButton button = new JButton(text);
-        button.addActionListener(this);
-        button.setAlignmentX(Component.CENTER_ALIGNMENT);
-        container.add(button);
-      }
-
-      public void reVisualize() {
-        visualizer.setPoly(points);
-        visualizer.repaint();
-      }
-
-      public class PolygonVisualizer extends JLabel {
-
-        private static final int SIZE = 100;
-        private static final int INSET = 5;
-        private Polygon myPoly;
-        private Polygon drawPoly;
-        private int selectedVertex = -1;
-
-        public PolygonVisualizer() {
-          setPreferredSize(new Dimension(SIZE, SIZE));
-        }
-
-        public PolygonVisualizer(ArrayList v) {
-          this();
-          setPoly(v);
-          repaint();
-        }
-
-        public void setPoly(ArrayList v) {
-          myPoly = new Polygon();
-          Iterator it = v.iterator();
-          while (it.hasNext()) {
-            Point p = (Point) it.next();
-            myPoly.addPoint(p.x, p.y);
-          }
-
-          // Shrink
-          Rectangle r = myPoly.getBounds();
-          int max = r.height > r.width ? r.height : r.width;
-          float factor = (float) max / (float) (SIZE - (INSET * 2));
-          drawPoly = new Polygon();
-          for (int i = 0; i < myPoly.npoints; i++) {
-            drawPoly.addPoint(
-              (int) (myPoly.xpoints[i] / factor),
-              (int) (myPoly.ypoints[i] / factor));
-          }
-
-          // And move
-          r = drawPoly.getBounds();
-          int dx = -r.x;
-          int dy = -r.y;
-          drawPoly.translate(dx+INSET, dy+INSET);
-        }
-
-        public void selectVertex(int vertex) {
-          selectedVertex = vertex;
-          repaint();
-        }
-
-        protected void paintComponent(Graphics g) {
-
-          g.drawPolygon(drawPoly);
-          if (selectedVertex >= 0 && selectedVertex < drawPoly.npoints) {
-            int x = drawPoly.xpoints[selectedVertex];
-            int y = drawPoly.ypoints[selectedVertex];
-            g.fillRect(x-2, y-2, 5, 5);
-          }
-
-        }
-      }
-
-      private void updateScrollPane() {
-
-        // Add a new Row if necessary
-        if (points.size() > rowPanels.size()) {
-          Row newRow = new Row(points.size() - 1, points, this);
-          scrollPanel.add(newRow);
-          rowPanels.add(newRow);
-        }
-
-        // Fill in the data
-        for (int i = 0; i < points.size(); i++) {
-          Point p = (Point) points.get(i);
-          Row row = (Row) rowPanels.get(i);
-          row.setVisible(true);
-          row.setRow(i);
-          row.setPos(p);
-          row.setHighlighted(selectedRow == i);
-        }
-
-        // Hide any extra rows
-        for (int i = points.size(); i < rowPanels.size(); i++) {
-          ((Row) rowPanels.get(i)).setVisible(false);
-        }
-
-        pack();
-        reVisualize();
-
-      }
-
-      public boolean isCancelled() {
-        return cancelled;
-      }
-
-      public ArrayList getPoints() {
-        return points;
-      }
-
-      public void selectRow(int row) {
-        if (row >= 0 && row <= points.size() - 1) {
-          if (rowPanels.size() > 0) {
-            ((Row) rowPanels.get(selectedRow)).setHighlighted(false);
-          }
-          ((Row) rowPanels.get(row)).setHighlighted(true);
-          selectedRow = row;
-          visualizer.selectVertex(row);
-        }
-      }
-
-      public void actionPerformed(ActionEvent e) {
-        String command = e.getActionCommand();
-
-        if (command.equals(OK_ACTION)) {
-          cancelled = false;
-          setVisible(false);
-        }
-        else if (command.equals(CANCEL_ACTION)) {
-          cancelled = true;
-          setVisible(false);
-        }
-        else if (command.equals(UP_ACTION)) {
-          if (selectedRow > 0) {
-            swapRows(selectedRow, selectedRow - 1);
-            selectRow(selectedRow - 1);
-          }
-        }
-        else if (command.equals(DOWN_ACTION)) {
-          if (selectedRow < points.size() - 1) {
-            swapRows(selectedRow, selectedRow + 1);
-            selectRow(selectedRow + 1);
-          }
-        }
-        else if (command.equals(ADD_ACTION)) {
-          if (selectedRow < 0) {
-            points.add(0, new Point(0, 0));
-            updateScrollPane();
-            selectRow(0);
-          }
-          else {
-            points.add(selectedRow, new Point((Point) points.get(selectedRow)));
-            updateScrollPane();
-          }
-
-        }
-        else if (command.equals(REMOVE_ACTION)) {
-          points.remove(selectedRow);
-          updateScrollPane();
-          if (selectedRow > points.size() - 1) {
-            selectedRow = points.size() - 1;
-          }
-          selectRow(selectedRow);
-        }
-      }
-
-      private void swapRows(int r1, int r2) {
-        Point p = (Point) points.get(r1);
-        points.set(r1, points.get(r2));
-        points.set(r2, p);
-        updateScrollPane();
-      }
-    }
-
-    protected class Row extends JPanel implements FocusListener {
-
-      protected int myRow;
-      protected ArrayList myPoints;
-      protected JTextField xPos, yPos;
-      protected Zone.PathConfig.PolygonConfigurer myConfig;
-
-      public void setHighlighted(boolean b) {
-        if (b) {
-          setBorder(BorderFactory.createBevelBorder(1));
-        }
-        else {
-          setBorder(null);
-        }
-      }
-
-      public Row(int i, ArrayList v, Zone.PathConfig.PolygonConfigurer c) {
-        myRow = i;
-        myPoints = v;
-        myConfig = c;
-
-        Point p = (Point) myPoints.get(myRow);
-
-        JLabel label = new JLabel(i + 1 + "");
-        add(label);
-
-        xPos = new JTextField(p.x + "", 5);
-        xPos.addFocusListener(this);
-        xPos.addKeyListener(new KeyAdapter() {
-          public void keyPressed(KeyEvent e) {
-            myConfig.selectRow(myRow);
-          }
-          public void keyReleased(KeyEvent e) {
-            try {
-              int val = Integer.valueOf(xPos.getText()).intValue();
-              Point myP = (Point) myPoints.get(myRow);
-              myP.x = val;
-              myConfig.reVisualize();
-            }
-            catch (Exception ev) {
-              //rolls[myRow].setPlus(0);
-            }
-          }
-        });
-        add(xPos);
-        yPos = new JTextField(p.y + "", 5);
-        yPos.addFocusListener(this);
-        yPos.addKeyListener(new KeyAdapter() {
-          public void keyPressed(KeyEvent e) {
-            myConfig.selectRow(myRow);
-          }
-          public void keyReleased(KeyEvent e) {
-            try {
-              int val = Integer.valueOf(yPos.getText()).intValue();
-              Point myP = (Point) myPoints.get(myRow);
-              myP.y = val;
-              myConfig.reVisualize();
-            }
-            catch (Exception ev) {
-              //rolls[myRow].setPlus(0);
-            }
-          }
-        });
-        add(yPos);
-
-      }
-
-      public void setPos(Point p) {
-        xPos.setText(p.x + "");
-        yPos.setText(p.y + "");
-      }
-
-      public void setRow(int row) {
-        myRow = row;
-      }
-
-      public void focusGained(FocusEvent e) {
-        myConfig.selectRow(myRow);
-      }
-      public void focusLost(FocusEvent e) {
-        //setHighlighted(false);
-      }
-    }
-*/
-
   public String[] getAttributeNames() {
     String s[] = {NAME, LOCATION_FORMAT, PATH};
     return s;
@@ -554,25 +150,6 @@ public class Zone extends AbstractConfigurable implements GridContainer {
     return myPolygon.contains(p);
   }
 
-//  private void checkRectangular() {
-//    if (pathPoints.size() == 4) {
-//      Point p1 = (Point) pathPoints.get(0);
-//      Point p2 = (Point) pathPoints.get(1);
-//      Point p3 = (Point) pathPoints.get(2);
-//      Point p4 = (Point) pathPoints.get(3);
-//      if (p1.x == p2.x) {
-//        if (p2.y == p3.y && p3.x == p4.x && p4.y == p1.y) {
-//          box = new Rectangle(p1.x, p1.y, (p4.x - p1.x), (p2.y - p1.y));
-//        }
-//      }
-//      else if (p1.y == p2.y) {
-//        if (p2.x == p3.x && p3.y == p4.y && p4.x == p1.x) {
-//          box = new Rectangle(p1.x, p1.y, (p2.x - p1.x), (p4.y - p1.y));
-//        }
-//      }
-//    }
-//  }
-
   /**
    * Snap to the grid in this zone,
    */
@@ -610,22 +187,6 @@ public class Zone extends AbstractConfigurable implements GridContainer {
     return myPolygon;
   }
 
-/*
-  public Shape getShape(double zoom) {
-    Polygon p = new Polygon();
-    for (int i = 0; i < myPolygon.npoints; i++) {
-      int x = (int) (myPolygon.xpoints[i] * zoom);
-      int y = (int) (myPolygon.ypoints[i] * zoom);
-      p.addPoint(x, y);
-    }
-    return p;
-  }
-
-  public Polygon getPolygon() {
-    return myPolygon;
-  }
-*/
-
   public Rectangle getBounds() {
     Rectangle r = myPolygon.getBounds();
     return r;
@@ -649,7 +210,8 @@ public class Zone extends AbstractConfigurable implements GridContainer {
   public static class Editor extends Configurer {
     private JButton button;
     private PolygonEditor editor;
-    private String path;
+    private Board board;
+    private JFrame frame;
 
     public Editor(final Zone zone) {
       super(PATH, null);
@@ -659,28 +221,17 @@ public class Zone extends AbstractConfigurable implements GridContainer {
           init(zone);
         }
       });
-    }
-
-    private void init(Zone zone) {
-      final Board background = zone.parentGrid.getBoard();
-      editor = new PolygonEditor(zone.myPolygon) {
+      editor = new PolygonEditor(new Polygon(zone.myPolygon.xpoints,zone.myPolygon.ypoints,zone.myPolygon.npoints)) {
         protected void paintBackground(Graphics g) {
-          if (background != null) {
-            background.draw(g, 0, 0, 1.0, editor);
+          if (board != null) {
+            board.draw(g, 0, 0, 1.0, editor);
           }
           else {
             super.paintBackground(g);
           }
         }
       };
-      if (background != null) {
-        background.fixImage(editor);
-      }
-      if (path != null) {
-        PolygonEditor.reset(editor.getPolygon(),path);
-      }
-      editor.setPreferredSize(background != null ? background.getSize() : new Dimension(600,600));
-      final JFrame frame = new JFrame(zone.getConfigureName());
+      frame = new JFrame(zone.getConfigureName());
       frame.getContentPane().setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS));
       JPanel labels = new JPanel();
       labels.setLayout(new GridLayout(2,2));
@@ -694,13 +245,20 @@ public class Zone extends AbstractConfigurable implements GridContainer {
       JButton closeButton = new JButton("Ok");
       closeButton.addActionListener(new ActionListener() {
         public void actionPerformed(ActionEvent e) {
-          path = PolygonEditor.polygonToString(editor.getPolygon());
-          frame.dispose();
+          setValue((Object) getValueString());
+          frame.setVisible(false);
         }
       });
       buttonPanel.add(closeButton);
       frame.getContentPane().add(buttonPanel);
+    }
 
+    private void init(Zone zone) {
+      board = zone.getBoard();
+      if (board != null) {
+        board.fixImage(editor);
+      }
+      editor.setPreferredSize(board != null ? board.getSize() : new Dimension(600,600));
       frame.pack();
       frame.setVisible(true);
     }
@@ -710,132 +268,12 @@ public class Zone extends AbstractConfigurable implements GridContainer {
     }
 
     public String getValueString() {
-      return path;
+      return PolygonEditor.polygonToString(editor.getPolygon());
     }
 
     public void setValue(String s) {
-      path = s;
+      PolygonEditor.reset(editor.getPolygon(),s);
     }
   }
-
-/*
-  public boolean isRectangle() {
-    return (box != null);
-  }
-
-  public void setShape(Rectangle r) {
-    ;
-  }
-
-*/
-/*
-  public void move(int dx, int dy, JComponent c) {
-    Rectangle oldRect = new Rectangle(getBounds());
-    myPolygon.translate(dx, dy);
-    c.repaint(oldRect.union(getBounds()));
-  }
-*/
-
-/*
-  public void drawGrid(
-    Graphics g,
-    Rectangle bounds,
-    Rectangle visibleRect,
-    double scale,
-    boolean reversed,
-    Component obs) {
-      if (grid != null) {
-        if (grid.isVisible()) {
-          grid.draw(g, bounds, visibleRect, scale, reversed, getShape(scale));
-        }
-        if (grid.getGridNumbering() != null
-          && grid.getGridNumbering().isVisible()) {
-          grid.getGridNumbering().draw(
-            g,
-            bounds,
-            visibleRect,
-            scale,
-            reversed,
-            getShape(scale));
-        }
-      }
-    }
-*/
-
-/*
-  public void draw(
-    Graphics g,
-    Rectangle bounds,
-    Rectangle visibleRect,
-    double scale,
-    boolean reversed,
-    Component obs) {
-
-    if (!bounds.intersects(visibleRect)) {
-      return;
-    }
-
-    drawGrid(g, bounds, visibleRect, scale, reversed, obs);
-
-    if (isHighlighted()) {
-      drawHighlight(g, bounds, visibleRect, scale, obs);
-    }
-  }
-
-*/
-/*
-  public void drawSelection(
-    Graphics g,
-    Rectangle bounds,
-    Rectangle visibleRect,
-    double scale,
-    Component obs) {
-
-    Point location = new Point(bounds.x, bounds.y);
-
-    getBoard().drawShape(g, location, visibleRect, scale, obs, getShape());
-    drawGrid(g, bounds, visibleRect, scale, board.isReversed(), obs);
-
-  }
-*/
-
-/*
-  public void drawHighlight(
-    Graphics g,
-    Rectangle bounds,
-    Rectangle visibleRect,
-    double scale,
-    Component obs) {
-
-    Graphics2D g2 = (Graphics2D) g;
-
-    Color oldColor = g2.getColor();
-    Stroke oldStroke = g2.getStroke();
-
-    g2.setColor(Color.RED);
-    g2.setStroke(new BasicStroke(2F));
-
-    g2.drawPolygon(getPolygon());
-
-    g2.setColor(oldColor);
-    g2.setStroke(oldStroke);
-  }
-
-  public void setSelected(boolean b) {
-    selected = b;
-  }
-
-  public boolean isSelected() {
-    return selected;
-  }
-
-  public void setHighlighted(boolean b) {
-    highlighted = b;
-  }
-
-  private boolean isHighlighted() {
-    return highlighted;
-  }
-*/
 
 }
