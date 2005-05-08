c51c2d780c25098055e6d2024a4a818b93af7257
swampwallaby <swampwallaby@67b53d14-2c14-4ace-a08f-0dab2b34000c>
2005-05-08 23:31:17 +0000

    *** empty log message ***
    git-svn-id: https://svn.code.sf.net/p/vassalengine/svn/trunk@591 67b53d14-2c14-4ace-a08f-0dab2b34000c

--- VSQL-src/UpFront/Messenger.java
+++ VSQL-src/UpFront/Messenger.java
@@ -23,8 +23,11 @@ import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import javax.swing.JPopupMenu;
+import javax.swing.KeyStroke;
 
 import VASSAL.build.AbstractConfigurable;
 import VASSAL.build.AutoConfigurable;
@@ -37,21 +40,26 @@ import VASSAL.build.module.documentation.HelpFile;
 import VASSAL.command.Command;
 import VASSAL.configure.Configurer;
 import VASSAL.configure.ConfigurerFactory;
+import VASSAL.configure.HotKeyConfigurer;
+import VASSAL.configure.IconConfigurer;
 import VASSAL.configure.PlayerIdFormattedStringConfigurer;
-import VASSAL.configure.StringArrayConfigurer;
+import VASSAL.configure.StringConfigurer;
 import VASSAL.tools.FormattedString;
-import VASSAL.tools.LaunchButton;
+import VASSAL.tools.KeyStrokeListener;
 import VASSAL.tools.PlayerIdFormattedString;
+
+import UpFront.StringArrayConfigurer;
 
 public class Messenger extends AbstractConfigurable implements ActionListener {
   
-  protected LaunchButton launch;
+  protected NewLaunchButton launch;
   protected JPopupMenu popup;
   protected Component owner;
   protected String[] messages = new String[0];
   protected FormattedString format = new PlayerIdFormattedString();
   Map map = null;
   protected String reportFormat = "$message$";
+  protected Messenger me;
 
   public static final String HOTKEY = "hotkey";
   public static final String BUTTON_TEXT = "text";
@@ -67,11 +75,12 @@ public class Messenger extends AbstractConfigurable implements ActionListener {
         selectAndSendMessage();
       }
     };
-    launch = new LaunchButton(null, BUTTON_TEXT, HOTKEY, ICON, al);
+    launch = new NewLaunchButton(null, BUTTON_TEXT, HOTKEY, ICON, al);
     setAttribute(NAME, "Messenger");
     setAttribute(BUTTON_TEXT, "Messages");
     launch.setToolTipText("Generate Counter Inventory");
     setConfigureName("Chat Messenger");
+    me = this;
   }
 
   public void addTo(Buildable b) {
@@ -83,7 +92,7 @@ public class Messenger extends AbstractConfigurable implements ActionListener {
          owner = GameModule.getGameModule().getFrame();
       }
       else {
-         owner = map.getView();
+         owner = map.getView();         
       }
     }
     else {
@@ -105,13 +114,6 @@ public class Messenger extends AbstractConfigurable implements ActionListener {
     return null;
   }
 
-//  /**
-//   * The name of this Configurable Object
-//   */
-//  public String getConfigureName() {
-//    return "Chat Messenger";
-//  }
-
   public Class[] getAllowableConfigureComponents() {
     return new Class[0];
   }
@@ -121,7 +123,7 @@ public class Messenger extends AbstractConfigurable implements ActionListener {
   }
 
   public Class[] getAttributeTypes() {
-    return new Class[] { String.class, String.class, IconConfig.class, String[].class, formatConfig.class };
+    return new Class[] { String.class, String.class, IconConfig.class, listConfig.class, formatConfig.class };
   }
 
   public String[] getAttributeNames() {
@@ -134,7 +136,13 @@ public class Messenger extends AbstractConfigurable implements ActionListener {
       return new PlayerIdFormattedStringConfigurer(key, name, new String[]{MESSAGE});
     }
   }
-      
+
+  public static class listConfig implements ConfigurerFactory {
+    public Configurer getConfigurer(AutoConfigurable c, String key, String name) {
+      return new StringArrayConfigurer(key, name, new String[]{MESSAGE});
+    }
+  }
+  
   public void setAttribute(String key, Object o) {
 
     if (NAME.equals(key)) {
@@ -176,11 +184,16 @@ public class Messenger extends AbstractConfigurable implements ActionListener {
     }
 
     int x = launch.getX() + ((map == null) ? 0 : launch.getParent().getBounds().x);
+    int y = launch.getY();
 
-    popup.show(owner, x, launch.getY());
+    popup.show(owner, x, y);
   }
   
   protected void buildPopup() {
+    
+    JMenu subMenu = null;
+    JMenuItem item;
+    
     popup = new JPopupMenu();
 
     popup.addPopupMenuListener(new javax.swing.event.PopupMenuListener() {
@@ -197,14 +210,33 @@ public class Messenger extends AbstractConfigurable implements ActionListener {
     });
 
     for (int i = 0; i < messages.length; i++) {
-      addItem(messages[i]);
+      String message = messages[i];
+      if (message.charAt(0) == '>') {
+        if (subMenu != null) {
+          popup.add(subMenu);
+        }
+        subMenu = new JMenu(message.substring(1));
+      }
+      else {
+        item = new JMenuItem(message);
+        item.addActionListener(this);
+        if (subMenu == null) {
+          popup.add(item);
+        }
+        else {
+          subMenu.add(item);  
+        }
+      }
+    }
+    if (subMenu != null) {
+      popup.add(subMenu);
     }
   }
 
-  protected void addItem(String item) {
+  protected void addItem(JMenu menu, String item) {
     JMenuItem messageItem = new JMenuItem(item);
     messageItem.addActionListener(this);
-    popup.add(messageItem);
+    menu.add(messageItem);
   }
 
   public void actionPerformed(ActionEvent e) {
@@ -216,5 +248,114 @@ public class Messenger extends AbstractConfigurable implements ActionListener {
     Command c = new Chatter.DisplayText(GameModule.getGameModule().getChatter(), formattedMessage);
     c.execute();
     GameModule.getGameModule().sendAndLog(c);
+  }
+ 
+  public class NewLaunchButton extends JButton {
+    private String nameAtt;
+    private String keyAtt;
+    private String iconAtt;
+    private IconConfigurer iconConfig;
+    private String toolTipText;
+    private KeyStrokeListener keyListener;
+    private Configurer nameConfig, keyConfig;
+
+    public NewLaunchButton(String name, String nameAttribute,
+                        String hotkeyAttribute, ActionListener al) {
+                        this(name,nameAttribute,hotkeyAttribute,null,al);
+    }
+
+    public NewLaunchButton(String name, String nameAttribute,
+                        String hotkeyAttribute, String iconAttribute, final ActionListener al) {
+      super(name);
+      nameAtt = nameAttribute;
+      keyAtt = hotkeyAttribute;
+      iconAtt = iconAttribute;
+      iconConfig = new IconConfigurer(iconAtt,null,null);
+      setAlignmentY(0.0F);
+      keyListener = new KeyStrokeListener(new ActionListener() {
+        public void actionPerformed(ActionEvent e) {
+          if (isEnabled() && isShowing()) {
+            al.actionPerformed(e);
+          }
+        }
+      });
+      GameModule.getGameModule().addKeyStrokeListener(keyListener);
+      addActionListener(al);
+    }
+
+    protected void fireActionPerformed(ActionEvent event) {
+      super.fireActionPerformed(event);
+      //GameModule.getGameModule().getChatter().getInputField().requestFocus();
+    }
+
+    public String getNameAttribute() {
+      return nameAtt;
+    }
+
+    public String getHotkeyAttribute() {
+      return keyAtt;
+    }
+
+    public String getIconAttribute() {
+      return iconAtt;
+    }
+
+    public String getAttributeValueString(String key) {
+      if (key.equals(nameAtt)) {
+        return getText();
+      }
+      else if (key.equals(keyAtt)) {
+        return HotKeyConfigurer.encode(keyListener.getKeyStroke());
+      }
+      else if (key.equals(iconAtt)) {
+        return iconConfig.getValueString();
+      }
+      else {
+        return null;
+      }
+    }
+
+    public void setAttribute(String key, Object value) {
+      if (key.equals(nameAtt)) {
+        setText((String) value);
+      }
+      else if (key.equals(keyAtt)) {
+        if (value instanceof String) {
+          value = HotKeyConfigurer.decode((String) value);
+        }
+        keyListener.setKeyStroke((KeyStroke) value);
+        setToolTipText(toolTipText);
+      }
+      else if (key.equals(iconAtt)) {
+        if (value instanceof String) {
+          iconConfig.setValue((String)value);
+          setIcon(iconConfig.getIconValue());
+        }
+      }
+    }
+
+    public void setToolTipText(String text) {
+      toolTipText = text;
+      if (keyListener.getKeyStroke() != null) {
+        text =
+          (text == null ? "" : text + " ")
+          + "[" + HotKeyConfigurer.getString(keyListener.getKeyStroke()) + "]";
+      }
+      super.setToolTipText(text);
+    }
+
+    public Configurer getNameConfigurer() {
+      if (nameConfig == null && nameAtt != null) {
+        nameConfig = new StringConfigurer(nameAtt, "Button text", getText());
+      }
+      return nameConfig;
+    }
+
+    public Configurer getHotkeyConfigurer() {
+      if (keyConfig == null && keyAtt != null) {
+        keyConfig = new HotKeyConfigurer(keyAtt, "Hotkey", keyListener.getKeyStroke());
+      }
+      return keyConfig;
+    }
   }
 }
---
+++ VSQL-src/VSQL/VSQLLabeler.java
@@ -0,0 +1,529 @@
+/*
+ * $Id$
+ *
+ * Copyright (c) 2000-2005 by Rodney Kinney, Brent Easton
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License (LGPL) as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, copies are available
+ * at http://www.opensource.org.
+ */
+package VSQL;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.Shape;
+import java.awt.event.InputEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.net.MalformedURLException;
+
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.DefaultListCellRenderer;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.KeyStroke;
+import javax.swing.ListCellRenderer;
+
+import VASSAL.build.GameModule;
+import VASSAL.build.module.documentation.HelpFile;
+import VASSAL.command.ChangeTracker;
+import VASSAL.command.Command;
+import VASSAL.configure.ColorConfigurer;
+import VASSAL.configure.FormattedStringConfigurer;
+import VASSAL.configure.IntConfigurer;
+import VASSAL.configure.StringConfigurer;
+import VASSAL.counters.Decorator;
+import VASSAL.counters.EditablePiece;
+import VASSAL.counters.GamePiece;
+import VASSAL.counters.KeyCommand;
+import VASSAL.counters.KeySpecifier;
+import VASSAL.counters.PieceEditor;
+import VASSAL.tools.FormattedString;
+import VASSAL.tools.SequenceEncoder;
+
+/** * Displays a text label, with content specified by the user at runtime */
+public class VSQLLabeler extends Decorator implements EditablePiece {
+  public static final String ID = "label;";
+  protected Color textBg = Color.black;
+  protected Color textFg = Color.white;
+
+  public static final int CENTER = 0;
+  public static final int RIGHT = 1;
+  public static final int LEFT = 2;
+  public static final int TOP = 3;
+  public static final int BOTTOM = 4;
+
+  public static int HORIZONTAL_ALIGNMENT = CENTER;
+  public static int VERTICAL_ALIGNMENT = TOP;
+
+  private String label = "";
+  private char labelKey = 'L';
+  private String menuCommand = "Change Label";
+  private Font font = new Font("Dialog", 0, 10);
+  private KeyCommand[] commands;
+  private FormattedString nameFormat = new FormattedString("$"+PIECE_NAME+"$ ($"+LABEL+"$)");
+  private static final String PIECE_NAME = "pieceName";
+  private static final String LABEL = "label";
+
+  private Image labelImage;
+  private JLabel lbl;
+  private char verticalJust = 'b';
+  private char horizontalJust = 'c';
+  private char verticalPos = 't';
+  private char horizontalPos = 'c';
+  private int verticalOffset = 0;
+  private int horizontalOffset = 0;
+
+  public VSQLLabeler() {
+    this(ID, null);
+  }
+
+  public VSQLLabeler(String s, GamePiece d) {
+    lbl = new JLabel();
+    mySetType(s);
+    setInner(d);
+  }
+
+  public void mySetType(String type) {
+    commands = null;
+    SequenceEncoder.Decoder st = new SequenceEncoder.Decoder(type, ';');
+    st.nextToken();
+    labelKey = st.nextChar('\0');
+    if (st.hasMoreTokens()) {
+      menuCommand = st.nextToken();
+      font = new Font("Dialog", Font.PLAIN, Integer.parseInt(st.nextToken()));
+      textBg = ColorConfigurer.stringToColor(st.nextToken());
+      Color c = ColorConfigurer.stringToColor(st.nextToken());
+      if (c != null) {
+        textFg = c;
+      }
+      verticalPos = st.nextChar('t');
+      verticalOffset = st.nextInt(0);
+      horizontalPos = st.nextChar('c');
+      horizontalOffset = st.nextInt(0);
+      verticalJust = st.nextChar('b');
+      horizontalJust = st.nextChar('c');
+      nameFormat.setFormat(st.nextToken("$"+PIECE_NAME+"$ ($"+LABEL+"$)"));
+    }
+    lbl.setForeground(textFg);
+    lbl.setFont(font);
+  }
+
+  public String myGetType() {
+    SequenceEncoder se = new SequenceEncoder(';');
+    se.append(labelKey == 0 ? "" : "" + labelKey);
+    se.append(menuCommand);
+    se.append("" + font.getSize());
+    String s = ColorConfigurer.colorToString(textBg);
+    se.append(s == null ? "" : s);
+    s = ColorConfigurer.colorToString(textFg);
+    se.append(s == null ? "" : s);
+    se.append(String.valueOf(verticalPos));
+    se.append(String.valueOf(verticalOffset));
+    se.append(String.valueOf(horizontalPos));
+    se.append(String.valueOf(horizontalOffset));
+    se.append(String.valueOf(verticalJust));
+    se.append(String.valueOf(horizontalJust));
+    se.append(nameFormat.getFormat());
+    return ID + se.getValue();
+  }
+
+  public String myGetState() {
+    return label;
+  }
+
+  public void mySetState(String s) {
+    setLabel(s.trim());
+  }
+
+  public String getName() {
+    if (label.length() == 0) {
+      return piece.getName();
+    }
+    else {
+      nameFormat.setProperty(PIECE_NAME,piece.getName());
+      nameFormat.setProperty(LABEL, label);
+      return nameFormat.getText();
+    }
+  }
+
+  public static void drawLabel(Graphics g, String text, int x, int y, int hAlign, int vAlign, Color fgColor, Color bgColor) {
+    drawLabel(g, text, x, y, new Font("Dialog", Font.PLAIN, 10), hAlign, vAlign, fgColor, bgColor, null);
+  }
+
+  public static void drawLabel(Graphics g, String text, int x, int y, Font f, int hAlign, int vAlign, Color fgColor, Color bgColor, Color borderColor) {
+    g.setFont(f);
+    int width = g.getFontMetrics().stringWidth(text + "  ");
+    int height = g.getFontMetrics().getHeight();
+    int x0 = x;
+    int y0 = y;
+    switch (hAlign) {
+      case CENTER:
+        x0 = x - width / 2;
+        break;
+      case LEFT:
+        x0 = x - width;
+        break;
+    }
+    switch (vAlign) {
+      case CENTER:
+        y0 = y - height / 2;
+        break;
+      case BOTTOM:
+        y0 = y - height;
+        break;
+    }
+    if (bgColor != null) {
+      g.setColor(bgColor);
+      g.fillRect(x0, y0, width, height);
+    }
+    if (borderColor != null) {
+      g.setColor(borderColor);
+      g.drawRect(x0, y0, width, height);
+    }
+    g.setColor(fgColor);
+    g.drawString(" " + text + " ", x0, y0 + g.getFontMetrics().getHeight() - g.getFontMetrics().getDescent());
+  }
+
+  public void draw(Graphics g, int x, int y, Component obs, double zoom) {
+    if (labelImage == null && label != null && label.length() > 0) {
+      labelImage = createImage(obs);
+    }
+    piece.draw(g, x, y, obs, zoom);
+
+    if (labelImage != null) {
+      Point p = getLabelPosition();
+      x += (int) (zoom * p.x);
+      y += (int) (zoom * p.y);
+      if (zoom != 1.0) {
+        Image scaled = labelImage;
+        scaled = GameModule.getGameModule().getDataArchive().getScaledImage(labelImage, zoom);
+        g.drawImage(scaled, x, y, obs);
+      }
+      else {
+        g.drawImage(labelImage, x, y, obs);
+      }
+    }
+  }
+
+  /**
+   * Return the relative position of the upper-left corner of the label, for a piece at position (0,0)
+   */
+  private Point getLabelPosition() {
+    int x = horizontalOffset;
+    int y = verticalOffset;
+
+    Rectangle selBnds = piece.getShape().getBounds();
+    switch (verticalPos) {
+      case 't':
+        y += selBnds.y;
+        break;
+      case 'b':
+        y += selBnds.y + selBnds.height;
+    }
+    switch (horizontalPos) {
+      case 'l':
+        x += selBnds.x;
+        break;
+      case 'r':
+        x += selBnds.x + selBnds.width;
+    }
+    switch (verticalJust) {
+      case 'b':
+        y -= lbl.getHeight();
+        break;
+      case 'c':
+        y -= lbl.getHeight() / 2;
+    }
+    switch (horizontalJust) {
+      case 'c':
+        x -= lbl.getWidth() / 2;
+        break;
+      case 'r':
+        x -= lbl.getWidth();
+    }
+    return new Point(x, y);
+  }
+
+  public void setLabel(String s) {
+    if (s == null) {
+      s = "";
+    }
+    label = s;
+    if (getMap() != null && label != null && label.length() > 0) {
+      labelImage = createImage(getMap().getView());
+    }
+    else {
+      labelImage = null;
+    }
+  }
+
+  public void setBackground(Color textBg) {
+    this.textBg = textBg;
+  }
+
+  public void setForeground(Color textFg) {
+    this.textFg = textFg;
+  }
+
+  protected Image createImage(Component obs) {
+    lbl.setText(label);
+    lbl.setSize(lbl.getPreferredSize());
+    int width = lbl.getWidth();
+    int height = lbl.getHeight();
+    BufferedImage im = new BufferedImage(width,height,BufferedImage.TYPE_4BYTE_ABGR);
+    Graphics g = im.createGraphics();
+    if (textBg != null) {
+      g.setColor(textBg);
+      g.fillRect(0, 0, width, height);
+    }
+    lbl.paint(g);
+    return im;
+  }
+
+  public String getLabel() {
+    return label;
+  }
+
+  public Rectangle boundingBox() {
+    Rectangle r = piece.boundingBox();
+    Rectangle r2 = piece.getShape().getBounds();
+    Point p2 = getLabelPosition();
+    Rectangle r3 = new Rectangle(p2, lbl.getSize());
+    return r.union(r2).union(r3);
+  }
+
+  public Shape getShape() {
+    return piece.getShape();
+  }
+
+  public KeyCommand[] myGetKeyCommands() {
+    if (commands == null) {
+      if (labelKey == 0) {
+        commands = new KeyCommand[0];
+      }
+      else {
+        commands = new KeyCommand[1];
+        commands[0] = new KeyCommand(menuCommand,
+                                     KeyStroke.getKeyStroke(labelKey, InputEvent.CTRL_MASK),
+                                     Decorator.getOutermost(this));
+      }
+    }
+    return commands;
+  }
+
+  public Command myKeyEvent(KeyStroke stroke) {
+    myGetKeyCommands();
+    Command c = null;
+    if (commands.length > 0
+        && commands[0].matches(stroke)) {
+      ChangeTracker tracker = new ChangeTracker(this);
+      String s = (String) JOptionPane.showInputDialog
+          (getMap() == null ? null : getMap().getView(),
+           commands[0].getName(),
+           null,
+           JOptionPane.QUESTION_MESSAGE,
+           null,
+           null,
+           label);
+      if (s == null) {
+        tracker = null;
+      }
+      else {
+        setLabel(s);
+        c = tracker.getChangeCommand();
+      }
+    }
+    return c;
+  }
+
+  public String getDescription() {
+    return "Text Label";
+  }
+
+  public HelpFile getHelpFile() {
+    File dir = VASSAL.build.module.Documentation.getDocumentationBaseDir();
+    dir = new File(dir, "ReferenceManual");
+    try {
+      return new HelpFile(null, new File(dir, "Label.htm"));
+    }
+    catch (MalformedURLException ex) {
+      return null;
+    }
+  }
+
+  public PieceEditor getEditor() {
+    return new Ed(this);
+  }
+
+  private static class Ed implements PieceEditor {
+    private KeySpecifier labelKeyInput;
+    private JPanel controls = new JPanel();
+    private StringConfigurer command;
+    private StringConfigurer initialValue;
+    private ColorConfigurer fg,bg;
+    private JComboBox hPos,vPos,hJust,vJust;
+    private IntConfigurer hOff,vOff,font;
+    private ListCellRenderer renderer;
+    private FormattedStringConfigurer format;
+
+    public Ed(VSQLLabeler l) {
+      controls.setLayout(new BoxLayout(controls, BoxLayout.Y_AXIS));
+
+      initialValue = new StringConfigurer(null, "Text", l.label);
+      controls.add(initialValue.getControls());
+
+      format = new FormattedStringConfigurer(null,"Name format",new String[]{PIECE_NAME,LABEL});
+      format.setValue(l.nameFormat.getFormat());
+      controls.add(format.getControls());
+
+      command = new StringConfigurer(null, "Menu Command", l.menuCommand);
+      controls.add(command.getControls());
+
+      Box b = Box.createHorizontalBox();
+      labelKeyInput = new KeySpecifier(l.labelKey);
+      b.add(new JLabel("Menu key command: "));
+      b.add(labelKeyInput);
+      controls.add(b);
+
+      font = new IntConfigurer(null, "Font size", new Integer(l.font.getSize()));
+      controls.add(font.getControls());
+
+      fg = new ColorConfigurer(null, "Text Color", l.textFg);
+      controls.add(fg.getControls());
+
+      bg = new ColorConfigurer(null, "Background Color", l.textBg);
+      controls.add(bg.getControls());
+
+      renderer = new MyRenderer();
+
+      Character[] rightLeft = new Character[]{new Character('c'),
+                                              new Character('r'),
+                                              new Character('l')};
+
+      Character[] topBottom = new Character[]{new Character('c'),
+                                              new Character('t'),
+                                              new Character('b')};
+
+      b = Box.createHorizontalBox();
+      b.add(new JLabel("Vertical position:  "));
+      vPos = new JComboBox(topBottom);
+      vPos.setRenderer(renderer);
+      vPos.setSelectedItem(new Character(l.verticalPos));
+      b.add(vPos);
+      vOff = new IntConfigurer(null, "Offset", new Integer(l.verticalOffset));
+      b.add(vOff.getControls());
+      controls.add(b);
+
+      b = Box.createHorizontalBox();
+      b.add(new JLabel("Horizontal position:  "));
+      hPos = new JComboBox(rightLeft);
+      hPos.setRenderer(renderer);
+      hPos.setSelectedItem(new Character(l.horizontalPos));
+      b.add(hPos);
+      hOff = new IntConfigurer(null, "Offset", new Integer(l.horizontalOffset));
+      b.add(hOff.getControls());
+      controls.add(b);
+
+      b = Box.createHorizontalBox();
+      b.add(new JLabel("Vertical text justification:  "));
+      vJust = new JComboBox(topBottom);
+      vJust.setRenderer(renderer);
+      vJust.setSelectedItem(new Character(l.verticalJust));
+      b.add(vJust);
+      controls.add(b);
+
+      b = Box.createHorizontalBox();
+      b.add(new JLabel("Horizontal text justification:  "));
+      hJust = new JComboBox(rightLeft);
+      hJust.setRenderer(renderer);
+      hJust.setSelectedItem(new Character(l.horizontalJust));
+      b.add(hJust);
+      controls.add(b);
+    }
+
+    public String getState() {
+      return initialValue.getValueString();
+    }
+
+    public String getType() {
+      SequenceEncoder se = new SequenceEncoder(';');
+      se.append(labelKeyInput.getKey());
+      se.append(command.getValueString());
+
+      Integer i = (Integer) font.getValue();
+      if (i == null
+          || i.intValue() <= 0) {
+        i = new Integer(10);
+      }
+      se.append(i.toString());
+      se.append(bg.getValueString());
+      se.append(fg.getValueString());
+      se.append(vPos.getSelectedItem().toString());
+      i = (Integer) vOff.getValue();
+      if (i == null) {
+        i = new Integer(0);
+      }
+      se.append(i.toString());
+      se.append(hPos.getSelectedItem().toString());
+      i = (Integer) hOff.getValue();
+      if (i == null) {
+        i = new Integer(0);
+      }
+      se.append(i.toString());
+      se.append(vJust.getSelectedItem().toString());
+      se.append(hJust.getSelectedItem().toString());
+      se.append(format.getValueString());
+      return ID + se.getValue();
+    }
+
+    public Component getControls() {
+      return controls;
+    }
+
+    private static class MyRenderer extends DefaultListCellRenderer {
+      public Component getListCellRendererComponent(JList list,
+                                                    Object value,
+                                                    int index,
+                                                    boolean sel,
+                                                    boolean focus) {
+        super.getListCellRendererComponent(list, value, index, sel, focus);
+        switch (((Character) value).charValue()) {
+          case 't':
+            setText("Top");
+            break;
+          case 'b':
+            setText("Bottom");
+            break;
+          case 'c':
+            setText("Center");
+            break;
+          case 'l':
+            setText("Left");
+            break;
+          case 'r':
+            setText("Right");
+        }
+        return this;
+      }
+    }
+  }
+}
