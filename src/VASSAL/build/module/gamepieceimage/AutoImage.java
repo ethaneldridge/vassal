fdc97a3e475dc9aeb9c184990044c60093ad27df
rodneykinney <rodneykinney@67b53d14-2c14-4ace-a08f-0dab2b34000c>
2005-11-28 06:21:55 +0000

    Game Piece Images:  integrating Brent's brilliant AutoImage stuff
    git-svn-id: https://svn.code.sf.net/p/vassalengine/svn/trunk@804 67b53d14-2c14-4ace-a08f-0dab2b34000c

---
+++ src/VASSAL/build/module/gamepieceimage/AutoImage.java
@@ -0,0 +1,237 @@
+/*
+ * $Id$
+ *
+ * Copyright (c) 2005 by Rodney Kinney, Brent Easton
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License (LGPL) as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, copies are available
+ * at http://www.opensource.org.
+ */
+package VASSAL.build.module.gamepieceimage;
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Rectangle;
+import java.awt.Shape;
+import java.awt.image.BufferedImage;
+
+import javax.swing.BoxLayout;
+import javax.swing.JPanel;
+import javax.swing.KeyStroke;
+
+import VASSAL.build.GameModule;
+import VASSAL.build.module.documentation.HelpFile;
+import VASSAL.command.ChangeTracker;
+import VASSAL.command.Command;
+import VASSAL.configure.StringConfigurer;
+import VASSAL.counters.Decorator;
+import VASSAL.counters.EditablePiece;
+import VASSAL.counters.GamePiece;
+import VASSAL.counters.KeyCommand;
+import VASSAL.counters.PieceEditor;
+import VASSAL.tools.SequenceEncoder;
+
+/** A Class that creates a generic counter based on a layout recorded
+ * in a GenericDefinition
+ */
+
+public class AutoImage extends Decorator  implements EditablePiece {
+
+  public static final String ID = "autoImage;";
+  public static final String PROPERTY_PREFIX = "AI_";
+  
+  protected static final String NAME = "name";
+  protected static final String DEFN_NAME = "defnName";
+  
+  protected String definitionName;
+  
+  protected GamePieceLayout layout = null;
+  protected GamePieceImage defn = null;
+  
+  protected BufferedImage image = null;
+  protected Rectangle imageBounds = new Rectangle();
+  
+  public AutoImage() {
+    this(ID, null);
+  }
+
+  public AutoImage(String type, GamePiece inner) {
+    mySetType(type);
+    setInner(inner);
+  }
+  
+  public AutoImage(String defnName) {
+    this();
+    definitionName = defnName;
+    buildImage();
+  }
+  
+  public void mySetState(String newState) {
+    if (defn != null) {
+      defn.setState(newState);
+      reBuildImage();
+    }
+  }
+
+  public String myGetState() {
+    if (defn == null) {
+      return "";
+    }
+    else {
+      return defn.getState();
+    }
+  }
+
+  public String myGetType() {
+    SequenceEncoder se = new SequenceEncoder(';');
+    se.append(definitionName);
+    return ID + se.getValue();
+  }
+
+  protected KeyCommand[] myGetKeyCommands() {
+    return defn.getKeyCommands(this);
+  }
+
+  public Command myKeyEvent(KeyStroke stroke) {
+    ChangeTracker change = new ChangeTracker(this);
+    defn.keyEvent(stroke);
+    reBuildImage();
+    return change.getChangeCommand();
+  }
+
+  public void draw(Graphics g, int x, int y, Component obs, double zoom) {
+    
+    piece.draw(g, x, y, obs, zoom);
+    
+    if (image == null) {
+      buildImage();
+    }
+
+    if (image != null) {
+      if (zoom == 1.0) {
+        g.drawImage(image, x + imageBounds.x, y + imageBounds.y, obs);
+      }
+      else {
+        Image scaledImage = GameModule.getGameModule().getDataArchive().getScaledImage(image, zoom);
+        g.drawImage(scaledImage,
+                    x + (int) (zoom * imageBounds.x),
+                    y + (int) (zoom * imageBounds.y),
+                    obs);
+      }
+    }
+  }
+  
+
+  public BufferedImage getImage() {
+    return image;
+  }
+  
+  public Rectangle getImageBounds() {
+    return new Rectangle(-image.getWidth()/2, -image.getHeight()/2, image.getWidth(), image.getHeight());
+  }
+  
+  protected void buildImage() {
+
+    /*
+     * Take a deep copy of the Image Definition to get our own copy of the
+     * Item instances to provide a counter context.
+     */
+    //if (defn == null) {
+      defn = (GamePieceImage) (GamePieceImageDefinitions.getInstance().getGenericDefn(definitionName));
+      if (defn == null) {
+        return;
+      }
+      defn = (GamePieceImage) defn.clone();
+      reBuildImage();
+  }
+  
+  protected void reBuildImage() {
+    if (defn != null) {
+      layout = defn.getLayout();
+      image = (BufferedImage) defn.getVisualizerImage();
+      if (image != null) {
+        int h = image.getHeight();
+        int w = image.getWidth();
+        imageBounds = new Rectangle(-w/2, -h/2, w, h);
+      }
+    }
+  }
+  
+  public Rectangle boundingBox() {
+    return new Rectangle(imageBounds.x, imageBounds.y, imageBounds.width, imageBounds.height);
+  }
+
+  public Shape getShape() {
+    return new Rectangle(imageBounds.x, imageBounds.y, imageBounds.width, imageBounds.height);
+  }
+
+
+  public PieceEditor getEditor() {
+    return new Ed(this);
+  }
+  
+  public String getName() {
+    String name = defn.getName(piece.getName());
+    return name;
+  }
+
+  public String getDescription() {
+    return "Auto Image";
+  }
+
+  public void mySetType(String type) {
+    type = type.substring(ID.length());
+    SequenceEncoder.Decoder st = new SequenceEncoder.Decoder(type, ';');
+    definitionName = st.nextToken("");
+    buildImage();
+  }
+
+  public HelpFile getHelpFile() {
+    return null;
+  }
+  
+  public Object getProperty(Object key) {
+    if (key instanceof String && ((String) key).startsWith(PROPERTY_PREFIX) && defn != null) {
+      return defn.getProperty(key);
+    }
+    return super.getProperty(key);
+  }
+  
+  protected static class Ed implements PieceEditor {
+    protected StringConfigurer definition;
+    protected  JPanel controls;
+    
+    protected Ed(AutoImage g) {
+      
+      controls = new JPanel();
+      controls.setLayout(new BoxLayout(controls, BoxLayout.Y_AXIS));
+
+      definition = new StringConfigurer(null, "Image Definition Name:  ", g.definitionName);
+      controls.add(definition.getControls());
+     
+    }
+
+    public Component getControls() {
+      return controls;
+    }
+
+    public String getType() {
+      SequenceEncoder se = new SequenceEncoder(';');
+      se.append(definition.getValueString());
+      return ID + se.getValue();
+    }
+
+    public String getState() {
+      return "";
+    }
+  }
+}
---
+++ src/VASSAL/build/module/gamepieceimage/ItemInstance.java
@@ -0,0 +1,219 @@
+/*
+ * $Id$
+ * 
+ * Copyright (c) 2005 by Rodney Kinney, Brent Easton
+ * 
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Library General Public License (LGPL) as published by
+ * the Free Software Foundation.
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Library General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; if not, copies are available at
+ * http://www.opensource.org.
+ */
+
+package VASSAL.build.module.gamepieceimage;
+
+import javax.swing.KeyStroke;
+
+import VASSAL.build.AbstractConfigurable;
+import VASSAL.build.Buildable;
+import VASSAL.build.module.documentation.HelpFile;
+import VASSAL.counters.GamePiece;
+import VASSAL.counters.KeyCommand;
+
+public abstract class ItemInstance extends AbstractConfigurable {
+
+  public static final String FG_COLOR = "fgColor";
+  public static final String BG_COLOR = "bgColor";
+  
+  protected String type = "";
+  protected String location = "";
+  protected ColorSwatch bgColor = ColorSwatch.getClear();
+  protected ColorSwatch fgColor = ColorSwatch.getBlack();
+  protected GamePieceImage defn;
+  protected Item item;
+  private String state = "";
+  
+  protected InstanceConfigurer myConfig = null;
+
+  public ItemInstance(String nam, String typ, String loc) {
+    setName(nam);
+    setType(typ);
+    setLocation(loc);
+  }
+
+  public ItemInstance() {
+    this("", "", "");
+  }
+
+  public ItemInstance(GamePieceImage defn) {
+    this();
+    this.defn = defn;
+  }
+
+  /*
+   * Generate a copy of the instance for use by the Generic trait.
+   */
+  
+  public ItemInstance statefulCopy() {
+    return this;
+  }
+  
+  protected void setItem() {
+    if (defn != null) {
+      GamePieceLayout layout = defn.getLayout();
+      if (layout != null) {
+        item = layout.getItem(name);
+      }
+    }
+  }
+  
+  public Item getItem() {
+    if (item == null) {
+      setItem();
+    }
+    return item;
+  }
+  
+  public void setConfig(InstanceConfigurer i) {
+    myConfig = i;
+  }
+  
+  public abstract String encode();
+
+  public static ItemInstance newDefaultInstance(String name, String type, String location) {
+
+    if (type.equals(SymbolItem.TYPE)) {
+      return new SymbolItemInstance(name, type, location, Symbol.NatoUnitSymbolSet.SZ_DIVISION,
+          Symbol.NatoUnitSymbolSet.INFANTRY, Symbol.NatoUnitSymbolSet.NONE);
+    }
+    else if (type.equals(TextItem.TYPE)) {
+      return new TextItemInstance(name, type, location, null);
+    }
+    else if (type.equals(ShapeItem.TYPE)) {
+      return new ShapeItemInstance(name, type, location);
+    }
+    else if (type.equals(ImageItem.TYPE)) {
+      return new ImageItemInstance(name, type, location);
+    }
+    return null;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setType(String type) {
+    this.type = type;
+  }
+
+  public String getType() {
+    return type;
+  }
+
+  public void setLocation(String location) {
+    this.location = location;
+  }
+
+  public String getLocation() {
+    return location;
+  }
+
+  public void setBgColor(ColorSwatch bgColor) {
+    this.bgColor = bgColor;
+  }
+
+  public ColorSwatch getBgColor() {
+    return bgColor;
+  }
+
+  public void setFgColor(ColorSwatch fgColor) {
+    this.fgColor = fgColor;
+  }
+
+  public ColorSwatch getFgColor() {
+    return fgColor;
+  }
+
+  public void setState(String state) {
+    return;
+  }
+
+  public String getState() {
+    return "";
+  }
+
+  public String[] getAttributeDescriptions() {
+    return new String[0];
+  }
+
+  public Class[] getAttributeTypes() {
+    return new Class[0];
+  }
+
+  public String[] getAttributeNames() {
+    return new String[0];
+  }
+
+  public void setAttribute(String key, Object value) {
+
+  }
+
+  public String getAttributeValueString(String key) {
+    return null;
+  }
+
+  public void removeFrom(Buildable parent) {
+
+  }
+
+  public HelpFile getHelpFile() {
+    return null;
+  }
+
+  public Class[] getAllowableConfigureComponents() {
+    return null;
+  }
+
+  public void addTo(Buildable parent) {
+    if (parent instanceof GamePieceImage) {
+      defn = (GamePieceImage) parent;
+    }
+  }
+
+  public int getKeyCommandCount() {
+    return 0;
+    
+  }
+  
+  public KeyCommand[] getKeyCommands(GamePiece target) {
+    return new KeyCommand[getKeyCommandCount()];
+  }
+  
+  public void keyEvent(KeyStroke stroke) {
+    return;
+  }
+
+  public String getSuffix() {
+    return "";
+  }
+
+  public String formatName(String name) {
+    return name;
+  }
+
+  public Object getProperty(Object key) {
+    return null;
+  }
+  
+}
