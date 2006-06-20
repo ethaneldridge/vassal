c7027f3cbe792802c469c1de45f459e0b8ce8a85
swampwallaby <swampwallaby@67b53d14-2c14-4ace-a08f-0dab2b34000c>
2006-06-20 12:37:21 +0000

    Chartmap1.4
    git-svn-id: https://svn.code.sf.net/p/vassalengine/svn/trunk@926 67b53d14-2c14-4ace-a08f-0dab2b34000c

--- VSQL-src/chartMap/MapWidget.java
+++ VSQL-src/chartMap/MapWidget.java
@@ -2,21 +2,31 @@ package chartMap;
 
 import java.awt.BorderLayout;
 import java.awt.Component;
+import java.awt.Point;
+import java.awt.dnd.DnDConstants;
+import java.awt.dnd.DropTargetDragEvent;
+import java.awt.dnd.DropTargetDropEvent;
+import java.awt.dnd.DropTargetEvent;
+import java.awt.dnd.DropTargetListener;
 
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTabbedPane;
 
 import org.w3c.dom.Element;
 
 import VASSAL.build.Buildable;
 import VASSAL.build.Widget;
+import VASSAL.build.module.map.PieceMover;
+import VASSAL.build.widget.TabWidget;
 
-public class MapWidget extends Widget {
+public class MapWidget extends Widget implements DropTargetListener {
 
   protected JPanel panel;
   protected JScrollPane mapScroll;
   protected WidgetMap map;
   protected Buildable parent;
+  protected JTabbedPane tab;
 
   public MapWidget() {
     panel = new JPanel();
@@ -27,19 +37,33 @@ public class MapWidget extends Widget {
     return "Map Widget";
   }
 
+  /*
+   * Maps must be built prior to game start, so force a rebuild
+   * immediately. Default for widgets is to defer build until
+   * first call to getComponent()
+   */
   public void build(Element e) {
     super.build(e);
-    rebuild();        // Force immediate build of the Map
+    rebuild();        
   }
   
+  /*
+   * Parent Widget has now completed building, so set up Drag Target
+   * handling if our parent is a TabWidget
+   */
   public Component getComponent() {
+    if (tab == null && parent instanceof TabWidget) {
+      tab = (JTabbedPane) ((TabWidget) parent).getComponent();
+      tab.setDropTarget(PieceMover.DragHandler.makeDropTarget(tab, DnDConstants.ACTION_MOVE, this));
+    }
     return panel;
   }
-  
-  public void AddTo(Buildable b) {
-    parent = b;
-  }
 
+  public void addTo(Buildable b) {
+    super.addTo(b);
+    parent = (Widget) b;
+  }
+  
   public void add(Buildable b) {
     if (b instanceof WidgetMap) {
       if (mapScroll != null) {
@@ -64,6 +88,32 @@ public class MapWidget extends Widget {
     super.remove(b);
   }
 
+  /*
+   * A counter has been dragged over the tab of a parent TabWidget
+   * containing another map, sp bring it to the front.
+   */
+  public void dragOver(DropTargetDragEvent e) {
+    if (tab != null) {
+      Point p = e.getLocation();
+      int tabNumber = tab.getUI().tabForCoordinate(tab, p.x, p.y);
+      if (tabNumber >= 0 && tabNumber != tab.getSelectedIndex()) {
+        tab.setSelectedIndex(tabNumber);
+      }
+    }
+  }
+  
+  public void dragEnter(DropTargetDragEvent e) {
+  }
+
+  public void dropActionChanged(DropTargetDragEvent e) {
+  }
+
+  public void drop(DropTargetDropEvent e) {
+  }
+
+  public void dragExit(DropTargetEvent e) {
+  }
+  
   public String[] getAttributeDescriptions() {
     return new String[] {"Name:  "};
   }
--- VSQL-src/chartMap/WidgetMap.java
+++ VSQL-src/chartMap/WidgetMap.java
@@ -1,18 +1,19 @@
 package chartMap;
 
+import java.awt.Point;
+
 import javax.swing.JComponent;
 import javax.swing.JScrollPane;
 
 import VASSAL.build.module.Map;
 import VASSAL.configure.VisibilityCondition;
 
-public class WidgetMap extends Map {
+public class WidgetMap extends Map implements Runnable {
 
-  
   public WidgetMap() {
     super();
   }
-  
+
   /*
    * Minimal setup - remove all docking and toolbar setup
    */
@@ -44,16 +45,41 @@ public class WidgetMap extends Map {
       return super.getAttributeVisibility(name);
     }
   }
-  
+
   public JComponent getView() {
     return super.getView();
   }
-  
+
   /*
    * Make the scroll pane accessible to the widget
    */
   public JScrollPane getScroll() {
-    return scroll; 
+    return scroll;
   }
-  
+
+  /*
+   * Delay 500ms before starting scroll
+   */
+  public void scrollAtEdge(Point evtPt, int dist) {
+    Point p = new Point(evtPt.x - scroll.getViewport().getViewPosition().x, evtPt.y - scroll.getViewport().getViewPosition().y);
+    int dx = 0, dy = 0;
+    if (p.x < dist && p.x >= 0)
+      dx = -1;
+    if (p.x >= scroll.getViewport().getSize().width - dist && p.x < scroll.getViewport().getSize().width)
+      dx = 1;
+    if (p.y < dist && p.y >= 0)
+      dy = -1;
+    if (p.y >= scroll.getViewport().getSize().height - dist && p.y < scroll.getViewport().getSize().height)
+      dy = 1;
+
+    if (dx != 0 || dy != 0) {
+      scroll(2 * dist * dx, 2 * dist * dy);
+    }
+  }
+
+  public void run() {
+    // TODO Auto-generated method stub
+
+  }
+
 }
