d70a116601a01afce2b89773f685becb837c64ad
swampwallaby <swampwallaby@67b53d14-2c14-4ace-a08f-0dab2b34000c>
2006-06-24 12:57:53 +0000

    *** empty log message ***
    git-svn-id: https://svn.code.sf.net/p/vassalengine/svn/trunk@930 67b53d14-2c14-4ace-a08f-0dab2b34000c

--- VSQL-src/vp/VPTracker.java
+++ VSQL-src/vp/VPTracker.java
@@ -19,10 +19,11 @@
 
 package vp;
 
-import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 import javax.swing.BoxLayout;
 import javax.swing.JDialog;
@@ -31,8 +32,10 @@ import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextArea;
 import javax.swing.KeyStroke;
-import javax.swing.border.TitledBorder;
+import javax.swing.border.LineBorder;
 import javax.swing.table.AbstractTableModel;
+import javax.swing.table.TableCellEditor;
+import javax.swing.table.TableCellRenderer;
 
 import VASSAL.build.AbstractConfigurable;
 import VASSAL.build.AutoConfigurable;
@@ -63,17 +66,15 @@ public class VPTracker extends AbstractConfigurable implements GameComponent, Un
   public static final String HOT_KEY = "hotkey";
   public static final String ICON = "icon";
   public static final String BUTTON_TEXT = "buttonText";
-  public static final String COLOR = "color";
   public static final String SIDES = "sides";
-
-  protected Color color = Color.white;
 
   protected VPWindow vpWindow;
   protected LaunchButton launch;
   protected JTextArea VPLabel = new JTextArea();
   protected String id;
   protected String[] sides = new String[0];
-  protected JTable table;
+  protected VPTable table;
+  protected ArrayList items = new ArrayList();
 
   public VPTracker() {
     
@@ -89,11 +90,30 @@ public class VPTracker extends AbstractConfigurable implements GameComponent, Un
     table = new VPTable();
   }
   
+  public void add(Buildable b) {
+    super.add(b);
+    if (b instanceof VPItem) {
+      VPItem item = (VPItem) b;
+      item.setColumnCount(sides.length);
+      item.setTable(table);
+      items.add(item);
+      table.restructure();
+    }
+  }
+  
+  public void remove(Buildable b) {
+    super.remove(b);
+    if (b instanceof VPItem) {
+      items.remove(b);
+      table.restructure();
+    }
+  }
+  
   /*
    * Module level Configuration stuff
    */
   public String[] getAttributeNames() {
-    return new String[] { NAME, BUTTON_TEXT, ICON, HOT_KEY, COLOR, SIDES };
+    return new String[] { NAME, BUTTON_TEXT, ICON, HOT_KEY, SIDES };
   }
 
   public void setAttribute(String key, Object value) {
@@ -101,18 +121,15 @@ public class VPTracker extends AbstractConfigurable implements GameComponent, Un
       setConfigureName((String) value);
       launch.setToolTipText((String) value);
     }
-    else if (COLOR.equals(key)) {
-      if (value instanceof String) {
-        value = ColorConfigurer.stringToColor((String) value);
-      }
-      color = (Color) value;
-    }
     else if (SIDES.equals(key)) {
       if (value instanceof String) {
         value = StringArrayConfigurer.stringToArray((String) value);
       }
       sides = ((String[]) value);
-      table.revalidate();
+      for (int i=0; i < items.size(); i++) {
+        getItemAt(i).setColumnCount(sides.length);
+      }
+      table.restructure();
     }
     else {
       launch.setAttribute(key, value);
@@ -123,9 +140,6 @@ public class VPTracker extends AbstractConfigurable implements GameComponent, Un
     if (NAME.equals(key)) {
       return getConfigureName() + "";
     }
-    else if (COLOR.equals(key)) {
-      return ColorConfigurer.colorToString(color);
-    }
     else if (SIDES.equals(key)) {
       return StringArrayConfigurer.arrayToString(sides);
     }
@@ -135,11 +149,11 @@ public class VPTracker extends AbstractConfigurable implements GameComponent, Un
   }
   
   public String[] getAttributeDescriptions() {
-    return new String[] { "Name:  ", "Button text:  ", "Button Icon:  ", "Hotkey:  ", "Background Color:  ", "Sides:  "};
+    return new String[] { "Name:  ", "Button text:  ", "Button Icon:  ", "Hotkey:  ", "Sides:  "};
   }
 
   public Class[] getAttributeTypes() {
-    return new Class[] { String.class, String.class, IconConfig.class, KeyStroke.class, Color.class, String[].class};
+    return new Class[] { String.class, String.class, IconConfig.class, KeyStroke.class, String[].class};
   }
 
   public static class IconConfig implements ConfigurerFactory {
@@ -164,7 +178,6 @@ public class VPTracker extends AbstractConfigurable implements GameComponent, Un
     idMgr.add(this);
  
     vpWindow = new VPWindow();    
-    vpWindow.setColor(color);
     vpWindow.pack(); 
 
   }
@@ -195,10 +208,27 @@ public class VPTracker extends AbstractConfigurable implements GameComponent, Un
   }
 
   protected void updateVPDisplay() {
-    vpWindow.setColor(color);
     vpWindow.repaint();
   }
 
+  protected int totalColumn(int col) {
+    int total = 0;
+    Iterator i = items.iterator();
+    while (i.hasNext()) {
+      total += ((VPItem) i.next()).getValues()[col];
+    }
+    return total;
+  }
+  
+  protected VPItem getItemAt(int i) {
+    if (i >=0 && i < items.size()) {
+      return (VPItem) items.get(i); 
+    }
+    else {
+      return null;
+    }
+  }
+  
   protected class VPWindow extends JDialog  {
 
     protected static final long serialVersionUID = 1L;
@@ -227,7 +257,7 @@ public class VPTracker extends AbstractConfigurable implements GameComponent, Un
       
       table = new VPTable();
       JScrollPane scroll = new JScrollPane(table, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-      scroll.setBorder(new TitledBorder(getConfigureName()));
+      scroll.setBorder(LineBorder.createGrayLineBorder());
       table.setPreferredScrollableViewportSize(new Dimension(400, 70));
       mainPanel.add(scroll);
       
@@ -243,8 +273,49 @@ public class VPTracker extends AbstractConfigurable implements GameComponent, Un
 
     private static final long serialVersionUID = 1L;
 
+    AbstractTableModel model;
+    
     public VPTable() {
-      setModel(new MyTableModel());
+      model = new MyTableModel(); 
+      setModel(model);
+      setColumnWidths();
+    }
+    
+    public void restructure() {
+      model.fireTableStructureChanged();
+      setColumnWidths();
+    }
+
+    protected void setColumnWidths() {
+      getColumnModel().getColumn(0).setPreferredWidth(160);
+      for (int i=1;i < model.getColumnCount(); i++) {
+        getColumnModel().getColumn(i).setPreferredWidth(80);  
+      }
+
+    }
+
+    public void updateTotals() {
+      model.fireTableRowsUpdated(items.size(), items.size());
+    }
+    
+    public void redisplay() {
+      repaint();
+    }
+    
+    public TableCellRenderer getCellRenderer(int row, int col) {
+      TableCellRenderer defaultRenderer = super.getCellRenderer(row, col);
+      if (col > 0 && row < items.size()) {
+          return getItemAt(row).getCellRenderer(col-1, defaultRenderer);
+      }
+      return defaultRenderer;
+    }
+    
+    public TableCellEditor getCellEditor(int row, int col) {
+      TableCellEditor defaultEditor = super.getCellEditor(row, col);
+      if (col > 0 && row < items.size()) {
+          return getItemAt(row).getCellEditor(col-1, defaultEditor);
+      }
+      return defaultEditor;
     }
     
     public class MyTableModel extends AbstractTableModel {
@@ -263,15 +334,68 @@ public class VPTracker extends AbstractConfigurable implements GameComponent, Un
         }
       }
       
+      /*
+       * Only display Totals column if more than one row
+       */
       public int getRowCount() {
-        // TODO Auto-generated method stub
-        return 1;
+        return items.size()+ (items.size() > 1 ? 1 : 0);
       }
-      public Object getValueAt(int arg0, int arg1) {
-        // TODO Auto-generated method stub
-        return null;
+      
+      public Object getValueAt(int row, int col) {
+        if (row == items.size()) {
+          if (col == 0) {
+            return "Total";
+          }
+          else {
+            return new Integer(totalColumn(col-1));
+          }
+        }
+        else {
+          if (col == 0) {
+            return getItemAt(row).getDescription();
+          }
+          else {
+            return new Integer(getItemAt(row).getValues()[col-1]);
+          }
+        }
       }
+
+      public void setValueAt(Object value, int row, int col) {      
+        if (row > 0 && row < items.size()) {
+          getItemAt(row).setValue(col-1, value);
+        }
+        table.updateTotals();
+      }
+      
+      public Class getColumnClass(int c) {
+        if (c == 0) {
+          return String.class;
+        }
+        return Integer.class;
+      }
+      
+      public boolean isCellEditable(int row, int col) {
+        if (col == 0 || row == items.size()) {
+          return false;
+        }
+        else {
+         return getItemAt(row).isEditable();
+        }
+      }
+      
     }
   }
-  
+
+  public interface VPItem {
+    
+    public String getDescription();
+    public int[] getValues();
+    public void setValue(int col, Object value);
+    public boolean isEditable();
+    public TableCellRenderer getCellRenderer(int column, TableCellRenderer dflt);
+    public TableCellEditor getCellEditor(int column, TableCellEditor dflt);
+    public void setColumnCount(int columns);
+    public void setTable(VPTable t);
+    
+  }
 }
