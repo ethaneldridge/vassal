c9324a35153a424fbaa93ab49fc28dec3fa9c00b
uckelman <uckelman@67b53d14-2c14-4ace-a08f-0dab2b34000c>
2010-11-05 22:48:23 +0000

    Merged george973@773:HEAD. Fixed Bug 2463: NPE in Map.getAttributeValueString()
    git-svn-id: https://svn.code.sf.net/p/vassalengine/svn/VASSAL-src/branches/3.1@7448 67b53d14-2c14-4ace-a08f-0dab2b34000c

--- src/VASSAL/build/module/Map.java
+++ src/VASSAL/build/module/Map.java
@@ -486,6 +486,8 @@ public class Map extends AbstractConfigurable implements GameComponent, MouseLis
     launchButton.setVisible(false);
     if (e != null) {
       super.build(e);
+      getBoardPicker();
+      getStackMetrics();
     }
     else {
       getBoardPicker();
