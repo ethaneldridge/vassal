b5ed22b48b817fcfcb77dbcff79d62094a106351
uckelman <uckelman@67b53d14-2c14-4ace-a08f-0dab2b34000c>
2011-09-12 20:18:44 +0000

    Merged pgeerkens@7773. Bug: If no gameModule (which happens in test beds), use the default speed instead of looking up module preferences.
    git-svn-id: https://svn.code.sf.net/p/vassalengine/svn/VASSAL-src/branches/3.1@7812 67b53d14-2c14-4ace-a08f-0dab2b34000c

--- src/VASSAL/tools/AdjustableSpeedScrollPane.java
+++ src/VASSAL/tools/AdjustableSpeedScrollPane.java
@@ -80,9 +80,15 @@ public class AdjustableSpeedScrollPane extends ScrollPane {
          }
       });
 
-      Prefs prefs = GameModule.getGameModule().getPrefs();
-      prefs.addOption(Resources.getString("Prefs.general_tab"), config); //$NON-NLS-1$
-      setSpeed(((Integer) prefs.getValue(SCROLL_SPEED)).intValue());
+      final GameModule g = GameModule.getGameModule();
+      if (g == null) {
+         setSpeed(defaultSpeed);
+      }
+      else {
+         final Prefs prefs = g.getPrefs();
+         prefs.addOption(Resources.getString("Prefs.general_tab"), config); //$NON-NLS-1$
+         setSpeed(((Integer) prefs.getValue(SCROLL_SPEED)).intValue());
+      }
    }
 
    private void setSpeed(int speed) {
